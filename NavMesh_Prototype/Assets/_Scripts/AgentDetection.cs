a6a893a6940c59c8ac143fbe82b057d3345d4ff1
Elias Rey <eliasrey12@gmail.com>
2022-05-20 20:46:12 +0200

    Rematado el 4.1.3 a falta del drawline

--- NavMesh_Prototype/Assets/_Scripts/AgentDetection.cs
+++ NavMesh_Prototype/Assets/_Scripts/AgentDetection.cs
@@ -8,6 +8,8 @@ public class AgentDetection : MonoBehaviour
     public Transform agentTarget;
     public Transform initialPoint;
     NavMeshAgent agent;
+
+    public Transform vision;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +20,22 @@ public class AgentDetection : MonoBehaviour
         RaycastHit hitData; // Info del rayo
         Vector3 directionToTarget = (agentTarget.position - transform.position).normalized; // Direccion del rayo
         int distanceToAgent = 15; // Distancia del rayo
-        float agentsDistance= Vector3.Distance(agentTarget.position, transform.position); // Distancia entre los dos agentes
+       // float agentsDistance= Vector3.Distance(agentTarget.position, transform.position); // Distancia entre los dos agentes
+
+        // Establecer limites de rotacion para limitar el angulo de vision del enemigo
+        float angle = Vector3.Angle(directionToTarget, transform.forward);
+        float angleLimit = 30f;
+
+        // Limites para dibujar los rayos
+        Vector3 leftLimit = Quaternion.AngleAxis(-angleLimit, transform.forward) * transform.forward;
+        Vector3 rightLimit = Quaternion.AngleAxis(angleLimit, transform.forward) * transform.forward;
 
         Debug.DrawRay(transform.position, directionToTarget * distanceToAgent, Color.yellow);
+        Debug.DrawRay(transform.position, leftLimit * 10, Color.red);
+        Debug.DrawRay(transform.position, rightLimit * 10, Color.green);
         
     // Si cumple el raycast y la distancia entre agentes es menor que la distancia del rayo
-        if(Physics.Raycast(transform.position, directionToTarget, out hitData, distanceToAgent, -1, QueryTriggerInteraction.Ignore) && hitData.collider.tag == "Agent"){
+        if(Physics.Raycast(transform.position, directionToTarget, out hitData, distanceToAgent, -1, QueryTriggerInteraction.Ignore) && hitData.collider.tag == "Agent" && angle < angleLimit){
             agent.destination = agentTarget.position;
             Debug.Log("Va al agent"); 
 
