f953340b9c633071e596a71e96a6bc53b32a9e3a
Elías Rey Morón <eliasrey12@gmail.com>
2022-05-23 22:23:28 +0200

    4.1.4 rematado a falta de ciertos problemas a la hora de perseguir al agente

--- NavMesh_Prototype/Assets/_Scripts/AgentDetection.cs
+++ NavMesh_Prototype/Assets/_Scripts/AgentDetection.cs
@@ -8,19 +8,56 @@ public class AgentDetection : MonoBehaviour
     public Transform agentTarget;
     public Transform initialPoint;
     NavMeshAgent agent;
+    AgentState state;
+    private Vector3 destination;
 
-    public Transform vision;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        state = AgentState.Idle;
+        destination = transform.position;
     }
 
     private void FixedUpdate() {
+
+        bool inRangeAndVisible = IsInRangeAndVisible();
+        
+        
+        switch(state) // Dicho switch llamará al metodo que establece los estados
+        {
+            case AgentState.Idle:
+                if(inRangeAndVisible){ 
+                    SetState(AgentState.Chasing);
+                }
+                break;
+            case AgentState.Chasing:
+                if(! inRangeAndVisible){
+                    SetState(AgentState.Returning);
+                }
+                else{
+                    destination = agentTarget.position;
+                }
+                break;
+            case AgentState.Returning:
+                if(inRangeAndVisible){
+                    SetState(AgentState.Chasing);
+                } else if(agent.isStopped){
+                    SetState(AgentState.Idle);
+                }
+                break;
+        }
+
+        agent.destination = destination; // En todo momento el agente tendrá un destino
+        
+    }
+
+
+
+    private bool IsInRangeAndVisible(){
         RaycastHit hitData; // Info del rayo
         Vector3 directionToTarget = (agentTarget.position - transform.position).normalized; // Direccion del rayo
-        int distanceToAgent = 15; // Distancia del rayo
-       // float agentsDistance= Vector3.Distance(agentTarget.position, transform.position); // Distancia entre los dos agentes
+        int distanceToAgent = 30; // Distancia del rayo
 
         // Establecer limites de rotacion para limitar el angulo de vision del enemigo
         float angle = Vector3.Angle(directionToTarget, transform.forward);
@@ -33,21 +70,38 @@ public class AgentDetection : MonoBehaviour
         Debug.DrawRay(transform.position, directionToTarget * distanceToAgent, Color.yellow);
         Debug.DrawRay(transform.position, leftLimit * 10, Color.red);
         Debug.DrawRay(transform.position, rightLimit * 10, Color.green);
-        
-    // Si cumple el raycast y la distancia entre agentes es menor que la distancia del rayo
-        if(Physics.Raycast(transform.position, directionToTarget, out hitData, distanceToAgent, -1, QueryTriggerInteraction.Ignore) && hitData.collider.tag == "Agent" && angle < angleLimit){
-            agent.destination = agentTarget.position;
-            Debug.Log("Va al agent"); 
 
-        } else {
-            Debug.Log("Va al punto inicial");
-            agent.destination = initialPoint.position;
+        // Devuelvo la condición: Si cumple el raycast, la distancia entre agentes es menor que la distancia del rayo y el angulo es menor que 30
+        return (Physics.Raycast(transform.position, directionToTarget, out hitData, distanceToAgent, -1, QueryTriggerInteraction.Ignore) && hitData.collider.tag == "Agent" && angle < angleLimit);
+
+    }
+    
+    /// <summary>
+    /// Metodo que llama un nuevo estado, y el agente realizará una acción u otra
+    /// </summary>
+    /// <param name="newState"> Nuevo estado </param>
+    void SetState(AgentState newState){
+        if(newState != state){
+            state = newState;
+            switch(newState){
+                case AgentState.Idle:
+                    destination = transform.position;
+                    break;
+                case AgentState.Chasing:
+                    destination = agentTarget.position;
+                    break;
+                case AgentState.Returning:
+                    destination = initialPoint.position; // Estado Return: Vuelve a su posicion inicial
+                    break;
+            }
         }
+        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //agent.destination = agentTarget.position;
-    }
+}
+
+public enum AgentState{
+    Idle, // Esperando
+    Chasing, // Persigue
+    Returning // Vuelve a su posicion
 }
